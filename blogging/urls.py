5745a322ebeff921a63fcf9d40ee1a777c7f4916
SelineCCC <seline0504.chen@gmail.com>
2022-06-01 00:08:51 -0700

    Initial

---
+++ blogging/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from blogging.views import detail_view, BlogListView
+
+urlpatterns = [
+    path('', BlogListView.as_view(), name="blog_index"),
+    path('posts/<int:post_id>', detail_view, name="blog_detail"),
+]
---
+++ blogging/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from blogging.models import Post
+from django.views.generic.list import ListView
+
+    
+'''
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    #template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    #body = template.render(context)
+    #return HttpResponse(body, content_type='text/html')
+    return render(request, 'blogging/list.html', context)
+'''
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
+
+
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
+
