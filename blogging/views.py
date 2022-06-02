004d971d379eddf447ae738efa064db489496eaf
SelineCCC <seline0504.chen@gmail.com>
2022-06-02 00:04:58 -0700

    found the bug and ready to submit

--- blogging/views.py
+++ blogging/views.py
@@ -5,38 +5,12 @@ from blogging.models import Post
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-    
-'''
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    #template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    #body = template.render(context)
-    #return HttpResponse(body, content_type='text/html')
-    return render(request, 'blogging/list.html', context)
-'''
 
 class BlogListView(ListView):
     queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
     template_name = 'blogging/list.html'
 
 
-'''
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
-'''
-
 class BlogDetailView(DetailView):
     queryset = Post.objects.exclude(published_date__exact=None)
     template_name = 'blogging/detail.html'
-
-    # def get_queryset(self, request, *args, **kwargs):
-    #     context = {'object': queryset}
-    #     return render(request, 'blogging/detail.html', context)
